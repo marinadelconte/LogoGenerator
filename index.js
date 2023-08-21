2fca8b6059764e95b42240aa45cc2fc90d61ce99
Marina Del Conte <marinadelconte@yahoo.com>
2023-08-21 19:45:42 -0400

    added info to render new logo to logo.svg file

--- index.js
+++ index.js
@@ -1,60 +1,64 @@
-const fs = require("fs")
 let SVG = require("./lib/svg")
-import inquirer from 'inquirer';
-import fs from "fs/promises";
-
-let example = new SVG("AND", "green", "triangle", "yellow")
-
+const inquirer = require('inquirer');
+const fs = require("fs");
+const { Circle, Square, Triangle } = require("./lib/shapes")
 
 
-let {shape, text, shapeColor, textColor} = await inquirer
-    .prompt([
-        {
-            type: 'list',
-            name: 'shape',
-            message: "Please select the shape you would like to use for your logo:",
-            choices: ['Circle', 'Triangle', 'Square'],
-        },
-        {
-            type: 'input',
-            name: 'text',
-            message: "Please provide up to three characters for your logo:",
-        },
-        {
-            type: 'input',
-            name: 'shapeColor',
-            message: "Please select a color for the shape by entering the word or the hexadecimal number:",
-        },
-        {
-            type: 'input',
-            name: 'textColor',
-            message: "Please select a color for the text by entering the word or the hexadecimal number:",
-        },
-        
-    ])
+const main = async () => {
+    let response = await inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'shape',
+                message: "Please select the shape you would like to use for your logo:",
+                choices: ['Circle', 'Triangle', 'Square'],
+            },
+            {
+                type: 'input',
+                name: 'text',
+                message: "Please provide up to three characters for your logo:",
+            },
+            {
+                type: 'input',
+                name: 'shapeColor',
+                message: "Please select a color for the shape by entering the word or the hexadecimal number:",
+            },
+            {
+                type: 'input',
+                name: 'textColor',
+                message: "Please select a color for the text by entering the word or the hexadecimal number:",
+            },
 
-let logoSVGContent = `
-${shape}
+        ])
 
-${textEntry(text)}
-
-${shapeColor}
-
-${textColor}
-`
-
-
-fs.writeFile("logo.svg", logoSVGContent, function(err){
-    if(err){
-        console.log(err)
+    console.log(response)
+    let shape;
+    if (response.shape === "Circle") {
+        shape = new Circle()
     }
-})
-
-
-function textEntry(text) {
-    if ( text > 3) {
-        return `Please provide no more than three characters for the logo.`
+    if (response.shape === "Square") {
+        shape = new Square()
     }
+    if (response.shape === "Triangle") {
+        shape = new Triangle()
+    }
+
+    shape.setColor(response.shapeColor);
+    let SVG = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+
+    ${shape.render()}
+
+    <text x="150" y="125" font-size="60" text-anchor="middle" fill="${response.textColor}">${response.text}</text>
+
+    </svg>`
+
+    console.log(SVG);
+    fs.writeFile("logo.svg", SVG, function (err) {
+        if (err) {
+            console.log(err)
+        }
+    })
 }
 
 
+main();
